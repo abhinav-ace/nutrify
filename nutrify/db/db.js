693a088d25141e5c69f8e33b59c972b27ab55e36
Abhinav Jain <72682700+abhinav-ace@users.noreply.github.com>
2021-01-20 16:02:10 +0530

    Add files via upload

---
+++ nutrify/db/db.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/nutrifyDB', {
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+  useFindAndModify : false,
+  useCreateIndex : true
+},(err) => {
+    if(err){
+        console.log("DB connection unsuccessful");
+    }
+    else{
+        console.log("DB connected");
+    }
+});
+
+var usersSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : "Required",
+        index : { unique: true }
+    },
+    email : {
+        type : String,
+        required : "Required",
+        index : { unique: true }
+    },
+    password : {
+        type : String,
+        required : "Required"
+    },
+    calorie : {
+        type : String,
+    },
+    meals : { 
+        type : Array,
+        default : []
+    },
+    isAdmin : { 
+        type: Boolean, 
+        default: false 
+    }
+});
+
+mongoose.model("users", usersSchema);
